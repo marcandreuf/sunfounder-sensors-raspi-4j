56e41d529fc57d1a5f3c69b8d90f3bdcfb17dbf1
Marc Andreu Fernandez <marcandreuf@gmail.com>
2015-05-19 22:57:45 +0100

    Adjust calculations for the NTC Thermistor KY_013.

--- src/main/java/org/mandfer/sunfunpi4j/Ex11_01_AnalogTempSensor.java
+++ src/main/java/org/mandfer/sunfunpi4j/Ex11_01_AnalogTempSensor.java
@@ -30,7 +30,21 @@ import com.pi4j.io.gpio.GpioFactory;
 import static java.lang.Math.log;
 
 /**
- *
+ * This code has been tunner with the NTC thermistor datasheet from Murata.
+ * http://www.murata.com/~/media/webrenewal/support/library/catalog/products/thermistor/ntc/r44e.ashx
+ * 
+ * The sample code has been adjusted from these forums:
+ * http://electronics.stackexchange.com/questions/8754/how-to-measure-temperature-using-a-ntc-thermistor
+ * 
+ * Because we are using an ADC0832 which is a 8 bit IC,
+ * the code has been adjusted to a value of 256 instead of 1024.
+ * 
+ * The sensor its been power at 3.3V and the GND and VCC has been switched, because the label on the
+ * board looks like is wrong. Based on this forum http://forum.arduino.cc/index.php/topic,209133.0.html
+ * "Temperature sensor module KY-013
+ * These modules are the same, they're all on boards labelled "S1". THE PINS ARE MARKED BACKWARDS ON THESE.
+ * Has pin 1 (marked S) (GND), pin 2 (5v power) and pin 3 (marked -) (analog signal.)"
+ * 
  * @author marcandreuf
  */
 public class Ex11_01_AnalogTempSensor extends ADC_Base {
@@ -55,24 +69,22 @@ public class Ex11_01_AnalogTempSensor extends ADC_Base {
     @Override
     protected void loop(String[] args) {        
         do {
-            double resistance = getTempfromThermister(get_ADC_Result());
-            logger.debug("Resistor value: "+resistance);
-            //logger.debug("Current temperature: "+temp);
+            double temp = getTempfromThermister(get_ADC_Result());
+            logger.debug("Current temperature: "+temp);
             delay(1000);
         } while (isNotInterrupted);
     }
     
     
-    private double getTempfromThermister(int rawADC) {
-        int KY_013Resistor = 10000;
-        double resisADC = ((256.0 / (double)rawADC)-1)*KY_013Resistor; 
+    private double getTempfromThermister(double rawADC) {
+        double KY_013Resistor = 10000;        
+        double b_constant = 3380.0;
+        double t0 = 298; // 273 + 25
+        double celciusAdjustment = 273.15;
+        double adc8BitPrecision = 256;
         
-        
-        double temp = log(((2560000/(double)rawADC) - 10000));
-        temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp ))* temp );
-        return temp - 273.15;// Convert Kelvin to Celcius
-        
-        //double farenh = 4050.00 / Math.log(resisADC/(10000.00*Math.exp(-4050.00/298.00)));
-        //return farenh - 273.15;
+        double resisADC = ((adc8BitPrecision / rawADC)-1) * KY_013Resistor;       
+        double farenh = b_constant / Math.log(resisADC/(KY_013Resistor*Math.exp(-b_constant/t0)));
+        return farenh - celciusAdjustment;
     }
 }
